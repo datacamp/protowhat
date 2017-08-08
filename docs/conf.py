117f8579ae311e5de35943812af0a6e2f5c3de6e
Michael Chow <machow@princeton.edu>
2017-08-08 14:36:34 -0400

    remove module names from docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ from unittest import mock
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 sys.path.insert(0, os.path.abspath('..'))
 
+add_module_names = False
 
 # -- General configuration ------------------------------------------------
 
