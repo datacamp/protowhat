2c655fde51551616145a616eb19ed20443eda9d2
Michael Chow <machow@princeton.edu>
2017-11-22 16:35:33 -0500

    allow check_file to get from dict of student code

--- protowhat/tests/test_check_files.py
+++ protowhat/tests/test_check_files.py
@@ -13,13 +13,22 @@ from protowhat.checks.check_funcs import check_node
 ast.Expr._priority = 0
 ast.Module._get_field_names = lambda self: self._fields
 ast.Expr._get_field_names = lambda self: self._fields
-DUMMY_AST = ast.parse("1 + 1")
 DUMMY_NODES = {'Expr': ast.Expr}
 
 class ParseHey:
     ParseError = Exception
 
-    def parse(self, *args, **kwargs): return DUMMY_AST
+    def parse(self, code, *args, **kwargs): return ast.parse(code)
+
+def assert_equal_ast(a, b):
+    assert ast.dump(a) == ast.dump(b)
+
+@pytest.fixture(scope="function")
+def tf():
+    with NamedTemporaryFile() as tmp:
+        tmp.file.write(b'1 + 1')
+        tmp.file.flush()
+        yield tmp
 
 @pytest.fixture(scope="function")
 def state():
@@ -34,25 +43,55 @@ def state():
         ast_dispatcher = Dispatcher(DUMMY_NODES, ParseHey())
         )
 
-def test_check_file(state):
-    with NamedTemporaryFile() as tf:
-        tf.file.write(b'content')
-        tf.file.flush()
-        child = cf.check_file(state, tf.name)
-        assert child.student_code == 'content'
-        assert child.student_ast == DUMMY_AST
+def test_check_file_use_fs(state, tf):
+    state.solution_code = { tf.name: '3 + 3' }
+    child = cf.check_file(state, tf.name, use_fs = True)
+    assert child.student_code == '1 + 1'
+    assert_equal_ast(child.student_ast, ast.parse(child.student_code))
+    assert child.solution_code == '3 + 3'
+    assert_equal_ast(child.solution_ast, ast.parse(child.solution_code))
+    assert check_node(child, 'Expr', 0)
+
+def test_check_file_use_fs_no_parse(state, tf):
+    state.solution_code = { tf.name: '3 + 3' }
+    child = cf.check_file(state, tf.name, parse = False, use_fs = True)
+    assert child.student_code == '1 + 1'
+    assert child.student_ast is None
+    assert child.solution_ast is None
+    with pytest.raises(TypeError):
         assert check_node(child, 'Expr', 0)
 
-def test_check_file_no_parse(state):
-    with NamedTemporaryFile() as tf:
-        tf.file.write(b'content')
-        tf.file.flush()
-        child = cf.check_file(state, tf.name, parse = False)
-        assert child.student_code == 'content'
-        assert child.student_ast is None
-        with pytest.raises(TypeError):
-            assert check_node(child, 'Expr', 0)
+def test_check_no_sol(state, tf):
+    child = cf.check_file(state, tf.name, use_fs = True, use_solution = False)
+    assert child.solution_code == None
 
 def test_check_dir(state):
     with TemporaryDirectory() as td:
         child = cf.test_dir(state, td)
+
+
+@pytest.fixture(scope="function")
+def code_state():
+    return State(
+        student_code = {'script1.py': '1 + 1', 'script2.py': '2 + 2'},
+        solution_code = {'script1.py': '3 + 3', 'script2.py': '4 + 4'},
+        reporter = Reporter(),
+        # args below should be ignored
+        pre_exercise_code = "NA", 
+        student_result = "", solution_result = "",
+        student_conn = None, solution_conn = None,
+        ast_dispatcher = Dispatcher(DUMMY_NODES, ParseHey())
+        )
+
+def test_check_file(code_state):
+    child = cf.check_file(code_state, 'script1.py')
+    assert child.student_code == "1 + 1"
+    assert_equal_ast(child.student_ast, ast.parse(child.student_code))
+    assert_equal_ast(child.solution_ast, ast.parse(child.solution_code))
+
+def test_check_file_no_parse(code_state):
+    child = cf.check_file(code_state, 'script1.py', parse = False)
+    assert child.student_code == "1 + 1"
+    assert child.student_ast is None
+    assert child.solution_ast is None
+
