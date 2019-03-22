4688f7a55845eb9e3b7b55f35a92ba0791222866
Jeroen Hermans <jeroen@datacamp.com>
2019-03-22 12:15:14 +0100

    Add helpers for common tree format
    Using DumpConfig it's easy to dump a tree to the format that can be restored to a tree compatible with the dispatcher by AstModule
    This makes it easy to adapt non-antlr trees to the dispatcher system (e.g. osh for shellwhat).

--- protowhat/utils_ast.py
+++ protowhat/utils_ast.py
@@ -1,4 +1,51 @@
 from ast import AST
+from collections import OrderedDict
+
+
+class DumpConfig:
+    def __init__(
+        self,
+        is_node=lambda node: isinstance(node, AST),
+        node_type=lambda node: node.__class__.__name__,
+        fields_iter=lambda node: node._fields,
+        field_val=lambda node, field: getattr(node, field, None),
+        is_list=lambda node: isinstance(node, list),
+        list_iter=id,
+        leaf_val=id,
+    ):
+        """
+        Configuration to convert a node tree to the dump format
+
+        The default configuration can be used to dump a tree of AstNodes
+        """
+        self.is_node = is_node
+        self.node_type = node_type
+        self.fields_iter = fields_iter
+        self.field_val = field_val
+        self.is_list = is_list
+        self.list_iter = list_iter
+        self.leaf_val = leaf_val
+
+
+def dump(node, config):
+    """
+    Convert a node tree to a simple nested dict
+
+    All steps in this conversion are configurable using DumpConfig
+
+    dump dictionary node: {"type": str, "data": dict}
+    """
+    if config.is_node(node):
+        fields = OrderedDict()
+        for name in config.fields_iter(node):
+            attr = config.field_val(node, name)
+            if attr is not None:
+                fields[name] = dump(attr, config)
+        return {"type": config.node_type(node), "data": fields}
+    elif config.is_list(node):
+        return [dump(x, config) for x in config.list_iter(node)]
+    else:
+        return config.leaf_val(node)
 
 
 class AstNode(AST):
@@ -40,6 +87,10 @@ class AstModule:
     @classmethod
     def parse(cls, code, **kwargs):
         raise NotImplementedError("This method needs to be defined in a subclass.")
+
+    @classmethod
+    def dump(cls, tree):
+        return dump(tree, DumpConfig())
 
     # methods below are for updating an AstModule subclass based on data in the dump dictionary format --
 
