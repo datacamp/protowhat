0e28b544a9bc1167aac31341a6b9fbebbbc302be
Jeroen Hermans <jeroen@datacamp.com>
2019-04-15 04:06:24 +0200

    Fix small formatting issues

--- example/example_helper.py
+++ example/example_helper.py
@@ -2,18 +2,21 @@ import ast
 import inspect
 import asttokens
 
+
 class PythonAst:
     def get_text(self, full_text=None):
-        atok = asttokens.ASTTokens(code, tree = self)
+        atok = asttokens.ASTTokens(code, tree=self)
         return atok.get_text(self)
+
 
 def patch_ast():
     # forces class to use PythonAst as a mixin
     # allows them to use get_text method
     for obj in ast.__dict__.values():
         if inspect.isclass(obj) and issubclass(obj, ast.AST):
-            if obj == ast.AST or PythonAst in obj.__bases__: continue
-            obj.__bases__ = obj.__bases__ + (PythonAst, )
+            if obj == ast.AST or PythonAst in obj.__bases__:
+                continue
+            obj.__bases__ = obj.__bases__ + (PythonAst,)
             obj._priority = 0
 
     # add AstNode, and ParseError classes for Dispatcher
