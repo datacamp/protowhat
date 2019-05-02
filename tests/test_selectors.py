eda0b3b270cbb07b6cc962b52d8cc3697a11c8e2
Jeroen Hermans <jeroen@datacamp.com>
2019-05-02 10:56:38 +0200

    Improve and test selector functionality

--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -1,16 +1,18 @@
 import pytest
 
-from protowhat.selectors import Selector, get_ord
+from protowhat.selectors import Selector, get_ord, DispatcherInterface, Dispatcher
+
+# use python's builtin ast library
+from ast import AST, Expr, Num
+Num._priority = 1
 
 
-def test_selector_standalone():
-    # use python's builtin ast library
-    from ast import Expr, Num
+@pytest.fixture
+def node():
+    return Expr(value=Num(n=1))
 
-    Expr._priority = 0
-    Num._priority = 1
-    node = Expr(value=Num(n=1))
 
+def test_selector_standalone(node):
     sel = Selector(Num)
     sel.visit(node)
     assert isinstance(sel.out[0], Num)
@@ -22,6 +24,30 @@ def test_selector_standalone():
     sel = Selector(Expr)
     sel.visit(node, head=True)
     assert len(sel.out) == 0
+
+
+@pytest.mark.parametrize(
+    "path_str, path_list",
+    [
+        ("", []),
+        ("a", ["a"]),
+        ("a.", ["a"]),
+        ("1", [1]),
+        ("a.0", ["a", 0]),
+        ("a.b.c", ["a", "b", "c"]),
+        ("a.2.c", ["a", 2, "c"]),
+    ],
+)
+def test_dispatcher_path_str_to_list(path_str, path_list):
+    assert DispatcherInterface._path_str_to_list(path_str) == path_list
+
+
+def test_dispatcher_find(node):
+    assert isinstance(Dispatcher(AST).find("Num", node)[0], Num)
+
+
+def test_dispatcher_select(node):
+    assert isinstance(Dispatcher(AST).select("value", node), Num)
 
 
 @pytest.mark.parametrize("num, ord", [(1, "first"), (12, "12th"), (23, "23rd")])
