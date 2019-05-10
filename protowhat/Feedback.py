f66a0743fbc5f95d57e034de706fb50d62d05e55
Jeroen Hermans <jeroen@datacamp.com>
2019-05-10 11:11:16 +0200

    Make Feedback compatible with pythonwhat

--- protowhat/Feedback.py
+++ protowhat/Feedback.py
@@ -8,7 +8,20 @@ class Feedback:
             self.highlighting_disabled = state.highlighting_disabled
 
     def _highlight_data(self):
-        return self.highlight.get_position()
+        if hasattr(self.highlight, "get_position"):
+            return self.highlight.get_position()
+        elif hasattr(self.highlight, "first_token") and hasattr(
+            self.highlight, "last_token"
+        ):
+            # used by pythonwhat
+            # a plugin+register system would be better
+            # if many different AST interfaces exist
+            return {
+                "line_start": self.highlight.first_token.start[0],
+                "column_start": self.highlight.first_token.start[1],
+                "line_end": self.highlight.last_token.end[0],
+                "column_end": self.highlight.last_token.end[1],
+            }
 
     def get_highlight_data(self):
         result = None
